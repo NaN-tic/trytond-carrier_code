347f12da81c46e482fb0bcb263721e7b1c5bb83e
resteve <resteve@zikzakmedia.com>
2015-12-01 11:03:57 +0100

    Tests

--- tests/test_carrier_code.py
+++ tests/test_carrier_code.py
@@ -1,39 +1,14 @@
-#!/usr/bin/env python
-#This file is part carrier_code module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
-
-import sys
-import os
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
-
+# This file is part of the carrier_code module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class CarrierCodeTestCase(unittest.TestCase):
-    '''
-    Test Carrier Code module.
-    '''
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('carrier_code')
-
-    def test0005views(self):
-        '''
-        Test views.
-        '''
-        test_view('carrier_code')
-
-    def test0006depends(self):
-        '''
-        Test depends.
-        '''
-        test_depends()
+class CarrierCodeTestCase(ModuleTestCase):
+    'Test Carrier Code module'
+    module = 'carrier_code'
 
 
 def suite():
@@ -41,6 +16,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         CarrierCodeTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
