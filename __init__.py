eaf3694fe640afe9ccd1174c5dbe7e696dae5a34
C?dric Krier <ced@b2ck.com>
2018-01-23 16:14:42 +0100

    Fix style

--- __init__.py
+++ __init__.py
@@ -1,11 +1,11 @@
-#This file is part carrier_code module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
+# This file is part carrier_code module for Tryton.  The COPYRIGHT file at the
+# top level of this repository contains the full copyright notices and license
+# terms.
 from trytond.pool import Pool
-from .carrier import *
+from . import carrier
 
 
 def register():
     Pool.register(
-        Carrier,
+        carrier.Carrier,
         module='carrier_code', type_='model')
